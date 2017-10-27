027a2b37b0177626da2448e61c81599d0c799a12
Hyunje Jun <me@noraesae.net>
2017-10-27 16:21:28 +0900

    Format project euler comment

--- examples/project-euler-39.js
+++ examples/project-euler-39.js
@@ -2,7 +2,8 @@ import { $ } from '../src/comp-op';
 
 /* Project Euler #39
 
-If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, there are exactly three solutions for p = 120.
+If p is the perimeter of a right angle triangle with integral length sides,
+{a,b,c}, there are exactly three solutions for p = 120.
 
 {20,48,52}, {24,45,51}, {30,40,50}
 
